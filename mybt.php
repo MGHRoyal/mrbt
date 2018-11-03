e6ef5d5e958a15208cf505b38d61c963c645a6c2
MGHRoyal <44735476+MGHRoyal@users.noreply.github.com>
2018-11-04 01:27:13 +0330

    Update mybt.php

--- mybt.php
+++ mybt.php
@@ -1,6 +1,6 @@
 <?php
 $url = "https://api.telegram.org/bot788164006:AAESFrkBBru23_t43qaVoGJgm3z6Uf5_41I/getUpdates";
 $result = file_get_contents($url);
-//var_dump($result);
+var_dump($result);
     file_put_contents("myBotResult.txt",$result);
 ?>
